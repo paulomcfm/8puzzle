207c5cca87686ce2ccf8796ca0880fd2ed50e4d9
Paulo Martin <paulomcfm@gmail.com>
2024-09-16 20:18:11 -0300

    Project finalized.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,15 @@ const App = () => {
   ]);
   const [finalGrid, setFinalGrid] = useState(null);
   const [level, setLevel] = useState('first');
-  const [shuffleCount, setShuffleCount] = useState(100);
+  const [shuffleCount, setShuffleCount] = useState(10);
+  const [totalTime, setTotalTime] = useState(0);
+  const [numberOfVisitedNodes, setNumberOfVisitedNodes] = useState(0);
+  const [pathSize, setPathSize] = useState(0);
+
+  const handleReset = () => {
+    window.location.reload();
+  };
+
 
   const valueToColor = {
     1: '#FFEB3B', // Yellow
@@ -32,7 +40,6 @@ const App = () => {
     '': '#FFFFFF' // White for empty square
   };
 
-  // Função para alterar o valor de um quadrado
   const handleChange = (id, newValue) => {
     const newGrid = grid.map((square) =>
       square.id === id ? { ...square, value: newValue === '' ? '' : parseInt(newValue) } : square
@@ -46,8 +53,11 @@ const App = () => {
     const isValid = requiredValues.every(value => values.includes(value));
 
     if (isValid) {
-      setFinalGrid(values);
-      console.log('Números salvos:', values);
+      const formattedFinalGrid = grid.map(square => ({
+        id: square.id,
+        value: square.value
+      }));
+      setFinalGrid(formattedFinalGrid);
     } else {
       alert('A grid deve conter exatamente os valores de 1 a 8 e um vazio, sem repetir.');
     }
@@ -72,6 +82,20 @@ const App = () => {
     return canMove[randomIndex];
   };
 
+  const animateShuffle = (shuffleSteps, stepIndex = 0) => {
+    if (stepIndex >= shuffleSteps.length) {
+      setGrid(shuffleSteps[shuffleSteps.length - 1]);
+      return;
+    }
+
+    const newGrid = shuffleSteps[stepIndex];
+    setGrid(newGrid);
+
+    setTimeout(() => {
+      animateShuffle(shuffleSteps, stepIndex + 1);
+    }, 500);
+  };
+
   const handleShuffle = () => {
     let newGrid = [...grid];
     const shuffleSteps = [];
@@ -81,103 +105,202 @@ const App = () => {
       const sorteado = sorteia(canMove);
       const emptyIndex = newGrid.findIndex(square => square.value === '');
       [newGrid[emptyIndex].value, newGrid[sorteado].value] = [newGrid[sorteado].value, newGrid[emptyIndex].value];
-      shuffleSteps.push({ newGrid: newGrid.map(square => ({ ...square })), emptyIndex, sorteado });
+      shuffleSteps.push(newGrid.map(square => ({ ...square })));
     }
-    
-    const animateShuffle = (stepIndex) => {
-      if (stepIndex >= shuffleSteps.length) {
-        setGrid(shuffleSteps[shuffleSteps.length - 1].newGrid);
-        console.log('Grid embaralhada:', shuffleSteps[shuffleSteps.length - 1].newGrid);
-        return;
-      }
 
-      const { newGrid, emptyIndex, sorteado } = shuffleSteps[stepIndex];
-      setGrid(newGrid);
-
-      setTimeout(() => {
-        animateShuffle(stepIndex + 1);
-      }, 500);
-    };
-    console.log('Grid Final:', finalGrid);
-    animateShuffle(0);
+    animateShuffle(shuffleSteps);
   };
 
-  const handleSolveBranchAndBound = () => {
-    const actualGrid = grid;
+  const isGridInPath = (grid, path) => {
+    return path.some(p => areGridsEqual(p, grid));
+  };
+
+  const handleBestFirst = () => {
+    setTotalTime(0);
+    setNumberOfVisitedNodes(0);
+    setPathSize(0);
+    let actualGrid = grid;
     const goalGrid = finalGrid;
-    let numberofVisitedNodes = 0;
+    let visitedNodes = 0;
     let startTime = new Date().getTime();
-    let endTime;
-    let timeElapsed;
-    let numberOfMoves = 0;
-    let lvl = 0;
-    let outOfOrder = getOutOfOrder(actualGrid, goalGrid);
     let queue = [];
-    let possibleMoves = [];
-    insertWithPriority({ grid: actualGrid, level: lvl, outOfOrder: outOfOrder }, queue);
-    if(actualGrid !== goalGrid) {
-      possibleMoves = getPossibleMoves(actualGrid);
+    let firstObject = { grid: actualGrid, outOfOrder: getOutOfOrder(actualGrid, goalGrid), path: [actualGrid] };
+    if (level === 'first') {
+      insertWithPriorityOutOfOrder(getPossibleMovesFirstLevel(firstObject), queue);
+    } else {
+      insertWithPriorityOutOfOrder(getPossibleMovesSecondLevel(firstObject), queue);
     }
-    console.log('Queue:', possibleMoves);
+    actualGrid = queue.shift();
+    while (!areGridsEqual(actualGrid.grid, goalGrid)) {
+      if (level === 'first') {
+        insertWithPriorityOutOfOrder(getPossibleMovesFirstLevel(actualGrid), queue);
+      } else {
+        insertWithPriorityOutOfOrder(getPossibleMovesSecondLevel(actualGrid), queue);
+      }
+      visitedNodes++;
+      actualGrid = queue.shift();
+    }
+    let totalTime = new Date().getTime() - startTime;
+    setTotalTime(totalTime/1000);
+    setNumberOfVisitedNodes(visitedNodes);
+    setPathSize(actualGrid.path.length);
+    animateShuffle(actualGrid.path);
   };
 
-  const getPossibleMoves = (actualGrid) => {
+  const areGridsEqual = (grid1, grid2) => {
+    for (let i = 0; i < grid1.length; i++) {
+      if (grid1[i].value !== grid2[i].value) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const getPossibleMovesFirstLevel = ({ grid: actualGrid, outOfOrder, currentLevel, path }) => {
     let listOfPossibleMoves = [];
     let emptyIndex = actualGrid.findIndex(square => square.value === '');
     let row = Math.floor(emptyIndex / 3);
     let col = emptyIndex % 3;
   
-    // Movimento para cima
+    const addMove = (newGrid, currentLevel) => {
+      if (!isGridInPath(newGrid, path)) {
+        listOfPossibleMoves.push({
+          grid: newGrid,
+          outOfOrder: getOutOfOrder(newGrid, finalGrid),
+          currentLevel: currentLevel + 1,
+          path: [...path, newGrid]
+        });
+      }
+    };
+  
     if (row > 0) {
       let newGrid = actualGrid.map(square => ({ ...square }));
       [newGrid[emptyIndex].value, newGrid[emptyIndex - 3].value] = [newGrid[emptyIndex - 3].value, newGrid[emptyIndex].value];
-      listOfPossibleMoves.push(newGrid);
+      addMove(newGrid, currentLevel);
     }
-  
-    // Movimento para baixo
     if (row < 2) {
       let newGrid = actualGrid.map(square => ({ ...square }));
       [newGrid[emptyIndex].value, newGrid[emptyIndex + 3].value] = [newGrid[emptyIndex + 3].value, newGrid[emptyIndex].value];
-      listOfPossibleMoves.push(newGrid);
+      addMove(newGrid, currentLevel);
     }
-  
-    // Movimento para a esquerda
     if (col > 0) {
       let newGrid = actualGrid.map(square => ({ ...square }));
       [newGrid[emptyIndex].value, newGrid[emptyIndex - 1].value] = [newGrid[emptyIndex - 1].value, newGrid[emptyIndex].value];
-      listOfPossibleMoves.push(newGrid);
+      addMove(newGrid, currentLevel);
     }
-  
-    // Movimento para a direita
     if (col < 2) {
       let newGrid = actualGrid.map(square => ({ ...square }));
       [newGrid[emptyIndex].value, newGrid[emptyIndex + 1].value] = [newGrid[emptyIndex + 1].value, newGrid[emptyIndex].value];
-      listOfPossibleMoves.push(newGrid);
+      addMove(newGrid, currentLevel);
     }
   
     return listOfPossibleMoves;
   };
 
-  const insertWithPriority = (node, queue) => {
-    let i = 0;
-    while (i < queue.length && queue[i].level < node.level) {
-      i++;
-    }
-    queue.splice(i, 0, node);
-  };
+  const getPossibleMovesSecondLevel = ({ grid: actualGrid, outOfOrder, currentLevel, path }) => {
+    let listOfPossibleMoves = [];
+    let emptyIndex = actualGrid.findIndex(square => square.value === '');
+    let row = Math.floor(emptyIndex / 3);
+    let col = emptyIndex % 3;
   
+    const addMove = (newGrid, currentLevel) => {
+      if (!isGridInPath(newGrid, path)) {
+        listOfPossibleMoves.push({ grid: newGrid, outOfOrder: getOutOfOrder(newGrid, finalGrid), currentLevel: currentLevel+1, path: [...path, newGrid] });
+      }
+    };
+  
+    if (row > 0) {
+      let newGrid = actualGrid.map(square => ({ ...square }));
+      [newGrid[emptyIndex].value, newGrid[emptyIndex - 3].value] = [newGrid[emptyIndex - 3].value, newGrid[emptyIndex].value];
+      addMove(newGrid, currentLevel);
+    }
+    if (row < 2) {
+      let newGrid = actualGrid.map(square => ({ ...square }));
+      [newGrid[emptyIndex].value, newGrid[emptyIndex + 3].value] = [newGrid[emptyIndex + 3].value, newGrid[emptyIndex].value];
+      addMove(newGrid, currentLevel);
+    }
+    if (col > 0) {
+      let newGrid = actualGrid.map(square => ({ ...square }));
+      [newGrid[emptyIndex].value, newGrid[emptyIndex - 1].value] = [newGrid[emptyIndex - 1].value, newGrid[emptyIndex].value];
+      addMove(newGrid, currentLevel);
+    }
+    if (col < 2) {
+      let newGrid = actualGrid.map(square => ({ ...square }));
+      [newGrid[emptyIndex].value, newGrid[emptyIndex + 1].value] = [newGrid[emptyIndex + 1].value, newGrid[emptyIndex].value];
+      addMove(newGrid, currentLevel);
+    }
+  
+    listOfPossibleMoves.forEach(move => {
+      let secondLevelMoves = getPossibleMovesFirstLevel({ grid: move.grid, outOfOrder: move.outOfOrder, currentLevel: move.currentLevel, path: move.path });
+      secondLevelMoves = secondLevelMoves.filter(secondMove => !areGridsEqual(secondMove.grid, actualGrid));
+      if (secondLevelMoves.length > 0) {
+        move.outOfOrder = Math.min(...secondLevelMoves.map(secondMove => secondMove.outOfOrder));
+      }
+    });
+  
+    return listOfPossibleMoves;
+  };
+
+  const insertWithPriorityOutOfOrder = (nodes, queue) => {
+    nodes.forEach(node => {
+      let i = 0;
+      while (i < queue.length && queue[i].outOfOrder <= node.outOfOrder) {
+        i++;
+      }
+      queue.splice(i, 0, node);
+    });
+  };
+
+  const insertWithPriorityOutOfOrderPlusLevel = (nodes, queue) => {
+    nodes.forEach(node => {
+      let i = 0;
+      while (i < queue.length && (queue[i].outOfOrder + queue[i].currentLevel) <= (node.outOfOrder + node.currentLevel)) {
+        i++;
+      }
+      queue.splice(i, 0, node);
+    });
+  }
+
   const getOutOfOrder = (actualGrid, finalGrid) => {
     let outOfOrder = 0;
     for (let i = 0; i < 9; i++) {
-      if (actualGrid[i] !== finalGrid[i]) {
+      if (actualGrid[i].value !== finalGrid[i].value) {
         outOfOrder++;
       }
     }
+    return outOfOrder;
   };
 
-
   const handleSolveA = () => {
-
+    setTotalTime(0);
+    setNumberOfVisitedNodes(0);
+    setPathSize(0);
+    let actualGrid = grid;
+    const goalGrid = finalGrid;
+    let visitedNodes = 0;
+    let startTime = new Date().getTime();
+    let queue = [];
+    let firstObject = { grid: actualGrid, outOfOrder: getOutOfOrder(actualGrid, goalGrid), currentLevel: 0, path: [actualGrid] };
+    if (level === 'first') {
+      insertWithPriorityOutOfOrderPlusLevel(getPossibleMovesFirstLevel(firstObject), queue);
+    } else {
+      insertWithPriorityOutOfOrderPlusLevel(getPossibleMovesSecondLevel(firstObject), queue);
+    }
+    actualGrid = queue.shift();
+    while (!areGridsEqual(actualGrid.grid, goalGrid)) {
+      if (level === 'first') {
+        insertWithPriorityOutOfOrderPlusLevel(getPossibleMovesFirstLevel(actualGrid), queue);
+      } else {
+        insertWithPriorityOutOfOrderPlusLevel(getPossibleMovesSecondLevel(actualGrid), queue);
+      }
+      visitedNodes++;
+      actualGrid = queue.shift();
+    }
+    let totalTime = new Date().getTime() - startTime;
+    setTotalTime(totalTime/1000);
+    setNumberOfVisitedNodes(visitedNodes);
+    setPathSize(actualGrid.path.length);
+    animateShuffle(actualGrid.path);
   };
 
   const selectFirstLevel = () => {
@@ -188,46 +311,53 @@ const App = () => {
     setLevel('second');
   };
 
-    return (
-      <Container className="mt-5">
-        <h2 className="text-center mb-4">Escolha o Estado Inicial</h2>
-        <Row className="justify-content-center">
-          <Col xs={12} md={4}>
-            <Row>
-              {grid.map((square, index) => (
-                <Col key={square.id} xs={4} className="border p-3 text-center square" data-id={index} style={{ height: "100px", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: valueToColor[square.value] }}>
-                  <Form.Control
-                    type="number"
-                    min="1"
-                    max="8"
-                    value={square.value}
-                    onChange={(e) => handleChange(square.id, e.target.value)}
-                    className="text-center"
-                  />
-                </Col>
-              ))}
-            </Row>
-            <Button className="mt-3" onClick={handleSave}>Definir Estado Final</Button>
-            <Form.Group className="mt-3">
-              <Form.Label>Embaralhar X vezes:</Form.Label>
-              <Form.Control
-                type="number"
-                value={shuffleCount}
-                onChange={(e) => setShuffleCount(e.target.value)}
-                className="text-center"
-              />
-            </Form.Group>
-            <Button className="mt-3" onClick={handleShuffle}>Embaralhar</Button>
-            <Button className={`mt-3 ml-3 ${level === 'first' ? 'btn-danger' : ''}`} onClick={selectFirstLevel}>1º Nível</Button>
-            <Button className={`mt-3 ml-3 ${level === 'second' ? 'btn-danger' : ''}`} onClick={selectSecondLevel}>2º Nível</Button>
-            <Button className="mt-3" onClick={handleSolveBranchAndBound}>Resolver Branch and Bound</Button>
-            <div className=""></div>
-            <Button className="mt-3" onClick={handleSolveA}>Resolver A*</Button>
+  return (
+    <Container className="mt-5">
+      <h2 className="text-center mb-4">Escolha o Estado Inicial</h2>
+      <Row className="justify-content-center">
+        <Col xs={12} md={4}>
+          <Row>
+            {grid.map((square, index) => (
+              <Col key={square.id} xs={4} className="border p-3 text-center square" data-id={index} style={{ height: "100px", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: valueToColor[square.value] }}>
+                <Form.Control
+                  type="number"
+                  min="1"
+                  max="8"
+                  value={square.value}
+                  onChange={(e) => handleChange(square.id, e.target.value)}
+                  className="text-center"
+                />
+              </Col>
+            ))}
+          </Row>
+          <Button className="mt-3" onClick={handleSave}>Definir Estado Final</Button>
+          {numberOfVisitedNodes !== 0 && (
+            <p>
+              Tempo total: {totalTime}s <br/>
+              Número de nós visitados: {numberOfVisitedNodes} <br/>
+              Tamanho do caminho da solução: {pathSize}
+            </p>
+          )}
+          <Button className="mt-3" onClick={handleReset}>Reset</Button>
+          <Form.Group className="mt-3">
+            <Form.Label>Embaralhar X vezes:</Form.Label>
+            <Form.Control
+              type="number"
+              value={shuffleCount}
+              onChange={(e) => setShuffleCount(e.target.value)}
+              className="text-center"
+            />
+          </Form.Group>
+          <Button className="mt-3" onClick={handleShuffle}>Embaralhar</Button>
+          <Button className={`mt-3 ml-3 ${level === 'first' ? 'btn-danger' : ''}`} onClick={selectFirstLevel}>1º Nível</Button>
+          <Button className={`mt-3 ml-3 ${level === 'second' ? 'btn-danger' : ''}`} onClick={selectSecondLevel}>2º Nível</Button>
+          <Button className="mt-3" onClick={handleBestFirst}>Resolver Best Frist</Button>
+          <div className=""></div>
+          <Button className="mt-3" onClick={handleSolveA}>Resolver A*</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-          </Col>
-        </Row>
-      </Container>
-    );
-  };
-
-  export default App;
+export default App;
